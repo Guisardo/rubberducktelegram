04534124df5bffa0f562341c48e2dd2909aef050
Lucas Rancez <lucas.rancez@fox.com>
2016-05-04 21:19:13 -0300

    added send reports by mail

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ var TelegramBot = require('node-telegram-bot-api'),
 var request = require('request');
 
 var duckCall = function(msg) {
-  console.log(msg);
   if (msg.text === '' || (msg.text.length > 0 && msg.text[0] !== '/')) {
     request('http://' + apihost + '/dialog?duck_id=telegram:' + msg.chat.id +
         '&answer=' + msg.text,
@@ -43,6 +42,63 @@ var duckCall = function(msg) {
   }
 };
 
+var duckReport = function(msg, match) {
+  var external_ducks = '';
+  if (match.length === 2 && match[1]) {
+    external_ducks = match[1];
+  }
+  request('http://' + apihost + '/fulldialog?duck_id=telegram:' + msg.chat.id +
+      '&external_ducks=' + external_ducks,
+      function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+          var jsResponse = JSON.parse(body);
+          var keyboard = [];
+          var botResponse = 'I\'d sent our conversation, ' +
+              'good luck with that. Be sure to check in the spam folder. ' +
+              'The mails from ducks don\'t get along with spam folders :) ' +
+              '(use /reset to start again)';
+          if (jsResponse.dialog.length <= 1) {
+            botResponse = 'Quack quack quack, quack quack. Quack?';
+            if (jsResponse.dialog.length === 1) {
+              setTimeout(function() {
+                bot.sendMessage(msg.chat.id,
+                    jsResponse.dialog[0].replace(/Q:/, ''), {
+                      'reply_markup': {
+                        'hide_keyboard': true
+                      }
+                  });
+              }, 1000);
+            }
+          } else if (!external_ducks) {
+            botResponse = 'You need to set a destination mail. ' +
+                'Ducks cannot read minds, you know? ' +
+                '(something like /report donald@duck.me)';
+            if (jsResponse.external_ducks) {
+              keyboard = [{
+                'text': '/report ' + jsResponse.external_ducks
+              }];
+            }
+          }
+          if (keyboard.length > 0) {
+            bot.sendMessage(msg.chat.id, botResponse, {
+              'reply_markup': {
+                'keyboard': [keyboard],
+                'resize_keyboard': true,
+                'one_time_keyboard': true,
+                'selective': true
+              }
+            });
+          } else {
+            bot.sendMessage(msg.chat.id, botResponse, {
+              'reply_markup': {
+                'hide_keyboard': true
+              }
+            });
+          }
+        }
+      });
+};
+
 bot.onText(/^\/start.*/, function(msg, match) {
   msg.text = '';
   duckCall(msg);
@@ -50,6 +106,9 @@ bot.onText(/^\/start.*/, function(msg, match) {
 bot.onText(/^\/reset.*/, function(msg, match) {
   msg.text = 'reset';
   duckCall(msg);
+});
+bot.onText(/^\/report(?: (.*))?/, function(msg, match) {
+  duckReport(msg, match);
 });
 
 bot.on('message', duckCall);
